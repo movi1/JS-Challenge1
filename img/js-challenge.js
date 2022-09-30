4f5baf05c6f5819e3ce5647db1e5477b5ec2e6c3
movi1 <98534830+movi1@users.noreply.github.com>
2022-09-30 13:14:03 +0100

    add javascript

--- img/js-challenge.js
+++ img/js-challenge.js
@@ -7,3 +7,19 @@ function maybeAlert() {
     alert("wooo let's go! :D");
 }
 
+var cart = document.getElementById('shopping-cart');
+cart.addEventListener("mouseover", myFunction);
+cart.addEventListener("click", mySecondFunction);
+cart.addEventListener("mouseout", myThirdFunction);
+
+function myFunction() {
+    document.getElementById("shops").innerHTML += "Moused over!<br>";
+  }
+  
+  function mySecondFunction() {
+    document.getElementById("shops").innerHTML += "Clicked!<br>";
+  }
+  
+  function myThirdFunction() {
+    document.getElementById("shops").innerHTML += "Moused out!<br>";
+  }
